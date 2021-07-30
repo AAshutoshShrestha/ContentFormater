16255289c430b0e7f5b87d2a5b7bd98f5a0dc434
Aashutosh Shrestha <aashutosh_1999@outlook.com>
2021-07-30 12:11:04 +0545

    Update content_gen.py

--- content_gen.py
+++ content_gen.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.sidebar.header("Content Formater ")
+st.sidebar.header("Content Formater For Product Post")
 
 
-location = st.sidebar.selectbox('Select the location', ['Kathmandu', 'Dharan', 'Birtamode'])
+location = st.sidebar.selectbox('Select the location: ', ['Kathmandu', 'Dharan', 'Birtamode'])
 st.markdown(''' 
-    ### Content will be generated of: ''')
+    ### Content Will be Generated For: ''')
 st.write(location)
 
 Status = st.sidebar.text_area('Write content here:') 
@@ -27,8 +27,7 @@ components.html("""<hr style="height:6px;border:none;color:#58CCED;background-co
 
 
 st.markdown(''' # Result ::''')
-
-st.markdown(''' ##### For Facebook ''')
+st.markdown(''' #### For Facebook ''')
 
 if location == 'Kathmandu':
     st.code(Status+"\n\nSTAY HOME 🏠 STAY SAFE😷 SHOP ONLINE 🛒 \n\n"+links+"\n\n\nEnjoy the biggest discounts and offers ever on groceries purchased online. 🛒🛒. \n\n✅No minimum order \n\n✅Discounts up to 20% \n\n✅2% cashback on eSewa payment \n\n✅Maximum delivery charge Rs. 50 \n\n✅Free Home Delivery above cart value of Rs. 1000 \n\n✅One lucky customer can win One plus Nord N100 Phone \n\n\nConditions:\n\n👉 Products available within Kathmandu Valley only\n\n✅Pay online, the swipe of a card at delivery is also available. We take cash on delivery but prefer not to.\n\nCall us on 📱9880123123 or Inbox us for any inquiry.\n\n#onlineshopping #saathimart #onlinebusiness #retail  #franchise \n\n#o2o #TogetherForChange #onlineshop #shoplocal #groceries  #lockdown #essentials")
@@ -39,8 +38,7 @@ else:
 
 
 st.markdown(''' ### Result ::''')
-
-st.markdown(''' ##### For Instagram ''')
+st.markdown(''' #### For Instagram ''')
 
 if location == 'Kathmandu':
     st.code(Status+"\n\nSTAY HOME 🏠 STAY SAFE😷 SHOP ONLINE 🛒 \n\n✔️To shop online please find the link in the bio. \n\n\nEnjoy the biggest discounts and offers ever on groceries purchased online. 🛒🛒. \n\n✅No minimum order \n\n✅Discounts up to 20% \n\n✅2% cashback on eSewa payment \n\n✅Maximum delivery charge Rs. 50 \n\n✅Free Home Delivery above cart value of Rs. 1000 \n\n✅One lucky customer can win One plus Nord N100 Phone \n\n\nConditions:\n\n👉 Products available within Kathmandu Valley only\n\n✅Pay online, the swipe of a card at delivery is also available. We take cash on delivery but prefer not to.\n\nCall us on 📱9880123123 or DM us for any inquiry.\n\n#onlineshopping #saathimart #onlinebusiness #retail  #franchise \n\n#o2o #TogetherForChange #onlineshop #shoplocal #groceries  #lockdown #essentials")
@@ -52,11 +50,10 @@ else:
 # for viber
 
 st.markdown(''' ### Result ::''')
-
-st.markdown(''' ##### For Viber Community ''')
+st.markdown(''' #### For Viber Community ''')
 
 if location == 'Kathmandu':
-    st.code(Status+"\n\nShop Now >>>>"+B_link) 
+    st.code(Status+"\n\nShop Now >>>>"+ B_link) 
 elif location == 'Birtamode':
     st.code(Status+"\n\nShop Now >>>> https://bit.ly/35KG0cR") 
 else:
